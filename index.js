0c48980bf3b3ae77fb2120fa543bc3ac5d1c6d4f
Markus Williams <markuswilliamsdev@gmail.com>
2020-02-13 03:50:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function happyCustomHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
-function holidayCountdown(holiiday, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`;
 }
